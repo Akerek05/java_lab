7ab1aa51fe12890f8b9d5842f2bd00851b9edf4f
Akerek05 <laszlo.kerekes2005@gmail.com>
2024-10-05 12:56:19 +0200

    all tasks done
    with chat gpt

--- labor4/src/labor4/main.java
+++ labor4/src/labor4/main.java
@@ -1,13 +1,19 @@
 package labor4;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.util.Scanner;
-public class main {
 
+public class main extends functions {
+
+	
 	public static void main(String[] args) throws IOException {
 		//Beer first = new Beer("Dreher","Ipa",5);
-				//Beer second = new Beer("Soproni","Világos",4);
-				//System.out.print(first.toString());
+		//Beer second = new Beer("Soproni","Világos",4);
+		//System.out.print(first.toString());
 		//System.out.print(second.toString()); 
 		 ArrayList<Beer> beers = new ArrayList<>();
 	        
@@ -17,25 +23,64 @@ public class main {
 	        do {
 	           
 	            
-	        	System.out.print("Enter something (type 'exit' to quit): ");
-	            input = scanner.nextLine();
-	            if(input.equals("exit")){continue;}
-	            String[] line = input.split(" ");
-		 		String function =line[0];
-		 		String name = line[1];
-		 		String style = line[2];
-		 		String strength =line[3];
-		 		double number = Double.parseDouble(strength);
-		 		Beer beer= new Beer(name,style,number);
-		 		beers.add(beer);
-		 		System.out.println("Size: "+beers.size());
-		 		System.out.println("added beer: "+beer.toString());		 		
-	        } while (!input.equalsIgnoreCase("exit"));
+	        	 System.out.print("Enter something (type 'exit' to quit): ");
+	             input = scanner.nextLine();
+	             if (input.equals("exit")) {
+	                 continue;
+	             }
+	             String[] line = input.split(" ");
+
+	             if (line[0].equals("add")) {
+	                 add(line, beers);
+	             } else if (line[0].equals("read")) {
+	                 if (line.length != 2) {
+	                     System.out.println("Usage: read <filename>");
+	                 } else {
+	                     readFromFile(line[1], beers);
+	                 }
+	             } else if (line[0].equals("write")) {
+	                 if (line.length != 2) {
+	                     System.out.println("Usage: write <filename>");
+	                 } else {
+	                     writeToFile(line[1], beers);
+	                 }
+	             } else if (line[0].equals("list")) {
+	                 if (line.length != 2) {
+	                     System.out.println("Usage: list <sortBy>");
+	                 } else {
+	                     list(beers, line[1]);
+	                 }
+	             } else if (line[0].equals("search")) {
+	                 if (line.length != 2) {
+	                     System.out.println("Usage: search <name>");
+	                 } else {
+	                     search(beers, line[1]);
+	                 }
+	             } else if (line[0].equals("find")) {
+	                 if (line.length != 2) {
+	                     System.out.println("Usage: find <substring>");
+	                 } else {
+	                     find(beers, line[1]);
+	                 }
+	             } else if (line[0].equals("delete")) {
+	                 if (line.length != 2) {
+	                     System.out.println("Usage: delete <name>");
+	                 } else {
+	                     delete(beers, line[1]);
+	                 }
+	             } else {
+	                 System.out.println("Unknown command. Please use 'add', 'read', 'write', 'list', 'search', 'find', or 'delete'.");
+	             }
+
+	         } while (!input.equalsIgnoreCase("exit"));
 
 	        scanner.close();
 		
 		 
 		 		System.exit(0);
-	}	 		
+	}
+	
+	 
+	
 
 }
